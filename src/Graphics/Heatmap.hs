4df61c71f6d81a3ea66e02b242bd1e1f394d900e
Dustin Sallings <dustin@spy.net>
2021-07-31 17:01:04 -0700

    Do some documenting.

--- src/Graphics/Heatmap.hs
+++ src/Graphics/Heatmap.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
 
-module Graphics.Heatmap where
+module Graphics.Heatmap (Point, mkDot, genHeatmap, mkTranslator) where
 
 import           Codec.Picture          (Image (..), PixelRGBA8 (..), generateImage)
 import           Control.Lens
@@ -12,6 +12,8 @@ import           Linear.V2              (V2 (..))
 import           Linear.Vector          (zero)
 import           Numeric.Natural
 
+-- | A Point as used throughout this heatmap codebase is currently
+-- limited to two dimensional vectors of natural numbers.
 type Point = V2 Natural
 
 class Projector a b | a -> b where
@@ -20,7 +22,13 @@ class Projector a b | a -> b where
 instance (Enum a, Integral a) => Projector (V2 a) a where
   project o size = [ V2 x y + o | x <- [ 0 .. size ], y <- [ 0 .. size ]]
 
+-- mkDot can be more generic, but we don't have a great use case for
+-- things other than V2 right now.
+--
 -- mkDot :: (Each a a Int Int, Projector a Int, Num a) =>  a -> Int -> [(a, Depth)]
+
+-- | Make a dot of a given size indicating presence that should begin
+-- heating the map.
 mkDot :: Point -> Natural -> [(Point, Depth)]
 mkDot o size = [ d | p2 <- project zero size,
                  d <- depthAt (o + p2) (distance p2) ]
@@ -31,12 +39,16 @@ mkDot o size = [ d | p2 <- project zero size,
       where f off = (fromIntegral off - (fromIntegral size / 2)) ^^ 2
     depthAt p d = [(p, maxBound - round (200 * d/middle + 50)) | d < middle]
 
+-- | Generate a heatmap image using a Colorizer and a collection of points and depths.
 genHeatmap :: (Natural, Natural) -> Colorizer -> [(Point, Depth)] -> Image PixelRGBA8
 genHeatmap (w,h) colorizer points = generateImage pf (fromIntegral w) (fromIntegral h)
   where
     m = Map.fromListWith (<>) points
     pf x y = colorizer $ Map.findWithDefault minBound (V2 (fromIntegral x) (fromIntegral y)) m
 
+-- | Make a scale translator that can translate points in an
+-- arbitrarily bounded two dimensional vector to our 2D vector with
+-- Natural bounds we use when applying points.
 mkTranslator :: RealFrac a => (V2 a, V2 a) -> (V2 Natural, V2 Natural) -> (V2 a -> V2 Natural)
 mkTranslator (V2 inminx inminy, V2 inmaxx inmaxy) (V2 outminx outminy, V2 outmaxx outmaxy) = t
   where
--- src/Graphics/Heatmap/Types.hs
+++ src/Graphics/Heatmap/Types.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeApplications           #-}
 
-module Graphics.Heatmap.Types where
+module Graphics.Heatmap.Types (Colorizer, ColorScheme, Depth(..), alphaColorizer, schemeColorizer) where
 
 import           Codec.Picture (PixelRGB8 (..), PixelRGBA8 (..))
 import qualified Data.Vector   as V
 import           Data.Word     (Word8)
 
+-- | A Colorizer produces a pixel for a given depth.
 type Colorizer = Depth -> PixelRGBA8
 
+-- | A ColorScheme is a collection of pixels that Depths can map to.
 type ColorScheme = [PixelRGBA8]
 
+-- | Depth represents how warm a particular spot is.
+--
+-- Depths as a semigroup may be added together, but can never have a
+-- value larger than 'maxBound'.
 newtype Depth = Depth Word8 deriving (Eq, Show, Num, Ord, Enum, Real, Integral, Bounded)
 
 instance Semigroup Depth where
@@ -21,11 +27,11 @@ instance Semigroup Depth where
 instance Monoid Depth where
   mempty = 0
 
--- Simple colorizer that uses a constant color and applies different alpha values.
+-- | Simple colorizer that uses a constant color and applies different alpha values.
 alphaColorizer :: PixelRGB8 -> Colorizer
 alphaColorizer (PixelRGB8 r g b) (Depth a) = PixelRGBA8 r g b a
 
--- Colorizer that applies a scheme.
+-- | Colorizer that applies a ColorScheme along with the depth.
 schemeColorizer :: ColorScheme -> Colorizer
 schemeColorizer = f . V.fromList
   where f v (Depth d) = PixelRGBA8 r g b d
